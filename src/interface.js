086b96dde5e88e8614cf13128c30fe62e1cc7ea6
DanielleInkster <danielle.inkster@gmail.com>
2019-10-16 15:24:21 +0100

    PSM off/on

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,44 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
-  $('#temperature').text(thermostat.temperature);
+  updateTemp();
 
   $('#temperature-up').on('click', function() {
     thermostat.upTemp()
-    $('#temperature').text(thermostat.temperature);
+    updateTemp()
   })
+
+  $('#temperature-down').on('click', function() {
+    thermostat.downTemp()
+    updateTemp()
+
+  })
+
+  $('#temperature-reset').on('click', function(){
+    thermostat.resetTemp()
+    updateTemp()
+  })
+
+  $('#powersavingmode-on').on('click', function(){
+    thermostat.turnPowerSaveOn()
+    $('#power-saving-status').text('on');
+  })
+
+  $('#powersavingmode-off').on('click', function(){
+    thermostat.turnPowerSaveOff()
+    $('#power-saving-status').text('off');
+  })
+
+
+
+
+
+
+
+
+
+  function updateTemp() {
+    $('#temperature').text(thermostat.temperature);
+  }
 
 });
