3a4754ebba454f3dffd985891979e38dfd766a2c
Vijay Kurian <kurianvijay7@gmail.com>
2019-10-16 15:48:33 +0100

    Added method for energy usage

--- src/interface.js
+++ src/interface.js
@@ -6,21 +6,25 @@ $(document).ready(function() {
   $('#temperature-up').on('click', function() {
     thermostat.upTemp()
     updateTemp()
+    energyUsage()
   })
 
   $('#temperature-down').on('click', function() {
     thermostat.downTemp()
     updateTemp()
-
+    energyUsage()
   })
 
   $('#temperature-reset').on('click', function(){
     thermostat.resetTemp()
     updateTemp()
+    energyUsage()
   })
 
   $('#powersavingmode-on').on('click', function(){
     thermostat.turnPowerSaveOn()
+    updateTemp()
+    energyUsage()
     $('#power-saving-status').text('on');
   })
 
@@ -29,16 +33,20 @@ $(document).ready(function() {
     $('#power-saving-status').text('off');
   })
 
-
-
-
-
-
-
+  function energyUsage() {
+    $('#energy-usage-status').text(thermostat.energyUsage())
+  }
 
 
   function updateTemp() {
     $('#temperature').text(thermostat.temperature);
+    if(thermostat.energyUsage() === 'low-usage') {
+      $('#temperature').css('color', 'green')
+    } else if(thermostat.energyUsage() === 'medium-usage') {
+      $('#temperature').css('color', 'black')
+    } else {
+      $('#temperature').css('color', 'red')
+    }
   }
 
 });
