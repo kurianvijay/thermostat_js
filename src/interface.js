c1361bbb9366ce0fe26f75a498bfbe8cf9a5e981
Vijay Kurian <kurianvijay7@gmail.com>
2019-10-16 15:06:06 +0100

    Added uptemp to buttons

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+
   $('#temperature').text(thermostat.temperature);
+
+  $('#temperature-up').on('click', function() {
+    thermostat.upTemp()
+    $('#temperature').text(thermostat.temperature);
+  })
+
 });
